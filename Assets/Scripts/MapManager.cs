49deab6950ba9073fb56b5eeac95beddc100dfde
Astoration <ybdh00@naver.com>
2017-07-19 06:49:34 +0900

    initialize map

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+	public GameObject[] particles;
+	public int density = 100;
+	public GameObject left,right,top,bottom;
+	// Use this for initialization
+	void Start () {
+		initializeMap ();
+	}
+
+	void initializeMap(){
+		for (float y = top.transform.position.y; y < bottom.transform.position; y += 1) {
+			for (int i = 0; i < density; i++) {
+				float x = Random.Range (left.transform.position.x, right.transform.position.x);
+				GameObject particle = Instantiate (particles [Random.Range (0, 2)]);
+				particle.transform.position = Vector2 (x, y);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
