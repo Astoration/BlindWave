fa3a1d64ac0a4a87e7e17ef4d6dd4a295113a22a
Astoration <ybdh00@naver.com>
2017-07-18 11:17:53 +0900

    implement player move control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float speed = 5f;
+	private Vector2 targetPoint;
 	// Use this for initialization
 	void Start () {
-		
+		targetPoint = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -16,10 +17,26 @@ public class PlayerController : MonoBehaviour {
 
 	void moveControl(){
 		#if UNITY_EDITOR
+		if(Input.GetMouseButtonDown(0)){
+			targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		}
+		#else
+		if(0<Input.touchCount){
+			Touch touch = Input.touches[0];
+			targetPoint = Camera.main.ScreenToWorldPoint(touch.position);
+		}
+		#endif
+		#if UNITY_EDITOR
 		var x = Input.GetAxis("Horizontal");
 		var y = Input.GetAxis("Vertical");
-		var axis = new Vector3(x,y);
+		var axis = new Vector2(x,y);
 		transform.Translate(axis * speed * Time.deltaTime);
+		if(axis.magnitude != 0)
+			targetPoint = transform.position;
 		#endif
+		#if UNITY_EDITOR
+		if(axis.magnitude == 0)
+		#endif
+		transform.position = Vector2.MoveTowards (transform.position, targetPoint,speed*Time.deltaTime);
 	}
 }
