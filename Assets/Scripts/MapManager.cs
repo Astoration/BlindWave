a8763e33cc0f7188008ae36d6be01ded4cdada94
Astoration <ybdh00@naver.com>
2017-07-19 08:28:07 +0900

    init density map

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour {
 	public GameObject[] particles;
-	public int density = 100;
+	public int density = 10;
 	public GameObject left,right,top,bottom;
 	// Use this for initialization
 	void Start () {
@@ -12,11 +12,11 @@ public class MapManager : MonoBehaviour {
 	}
 
 	void initializeMap(){
-		for (float y = top.transform.position.y; y < bottom.transform.position.y; y += 1) {
+		for (float y = bottom.transform.position.y; y < top.transform.position.y; y += 1000/density) {
 			for (int i = 0; i < density; i++) {
 				float x = Random.Range (left.transform.position.x, right.transform.position.x);
 				GameObject particle = Instantiate (particles [Random.Range (0, 2)]);
-				particle.transform.position = new Vector2 (x, y);
+				particle.transform.position = new Vector2 (x, y+Random.Range(-5f,5f));
 			}
 		}
 	}
