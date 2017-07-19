75899d201361c915534c38c38937d885a8fafac7
Astoration <ybdh00@naver.com>
2017-07-19 09:07:15 +0900

    add echo component

---
+++ Assets/Scripts/Echo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Echo : MonoBehaviour {
+	public float lifeTime;
+	float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTime -= Time.deltaTime;
+		transform.localScale = transform.localScale + speed * Time.deltaTime;
+		if (lifeTime < 0)
+			Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour {
+	public Material defaultMaterial;
+	public Material lightingMaterial;
+	private SpriteRenderer renderer;
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("echo")) {
+			this.renderer.material = defaultMaterial;
+		}
+	}
+}
