ec0f0c4eb119674b7effe603e4bcabeaedb1103a
Astoration <ybdh00@naver.com>
2017-07-19 11:59:07 +0900

    finished how to

--- Assets/Scripts/TitleSceneManager.cs
+++ Assets/Scripts/TitleSceneManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleSceneManager : MonoBehaviour {
-	public GameObject difficultMenu;
+	public GameObject difficultMenu, howto1, howto2;
 	// Use this for initialization
 	void Start () {
 		
@@ -21,9 +21,24 @@ public class TitleSceneManager : MonoBehaviour {
 
 	public void closeMenu(){
 		difficultMenu.SetActive (false);
+		howto1.SetActive (false);
+		howto2.SetActive (false);
 	}
 
 	public void startGame(int level){
 		SceneManager.LoadScene ("InGameScene");
+	}
+
+	public void displayHowTo(){
+		howto1.SetActive (true);
+	}
+
+	public void OnNext(){
+		howto1.SetActive (false);
+		howto2.SetActive (true);
+	}
+	public void OnFinish(){
+		howto2.SetActive (false);
+		difficultMenu.SetActive (true);
 	}
 }
